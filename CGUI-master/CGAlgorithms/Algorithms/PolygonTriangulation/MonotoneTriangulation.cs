6334d0c9585c361a351d55e2bf5df5f7179c6cf7
MohammedElsayeed <muhammedahmedelsayeed@gmail.com>
2024-12-23 18:26:42 +0200

    sweep && monotone

--- CGUI-master/CGAlgorithms/Algorithms/PolygonTriangulation/MonotoneTriangulation.cs
+++ CGUI-master/CGAlgorithms/Algorithms/PolygonTriangulation/MonotoneTriangulation.cs
@@ -3,14 +3,167 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CGUtilities;
 
 namespace CGAlgorithms.Algorithms.PolygonTriangulation
 {
-    class MonotoneTriangulation  :Algorithm
+    class MonotoneTriangulation : Algorithm
     {
-        public override void Run(System.Collections.Generic.List<CGUtilities.Point> points, System.Collections.Generic.List<CGUtilities.Line> lines, System.Collections.Generic.List<CGUtilities.Polygon> polygons, ref System.Collections.Generic.List<CGUtilities.Point> outPoints, ref System.Collections.Generic.List<CGUtilities.Line> outLines, ref System.Collections.Generic.List<CGUtilities.Polygon> outPolygons)
+        public override void Run(List<Point> inputPoints, List<Line> inputLines, List<Polygon> inputPolygons, ref List<Point> outputPoints, ref List<Line> outputLines, ref List<Polygon> outputPolygons)
         {
+            Polygon polygon = new Polygon(inputLines);
+            polygon = MakePolygonCounterClockwise(polygon);
+            bool isMonotone = CheckMonotone(polygon);
 
+            for (int i = 0; i < polygon.lines.Count; i++)
+                outputPoints.Add(polygon.lines[i].Start);
+
+            // Sort the points based on max Y and max X in case of a tie O(n)
+            List<Point> sortedPoints = new List<Point>();
+            for (int i = 0; i < polygon.lines.Count; i++)
+                sortedPoints.Add(polygon.lines[i].Start);
+            sortedPoints.Sort(PointSort);
+
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(sortedPoints[0]);
+            stack.Push(sortedPoints[1]);
+            TriangulateMonotonePolygon(outputPoints, outputLines, polygon, sortedPoints, stack);
+        }
+
+        private void TriangulateMonotonePolygon(List<Point> outputPoints, List<Line> outputLines, Polygon polygon, List<Point> sortedPoints, Stack<Point> stack)
+        {
+            int current = 2;
+
+            while (current != polygon.lines.Count)
+            {
+                Point currentPoint = sortedPoints[current];
+                Point top = stack.Peek();
+
+                bool sameSide = CheckSameSide(sortedPoints, currentPoint, top);
+
+                if (sameSide)
+                {
+                    current = HandleSameSide(outputPoints, outputLines, polygon, stack, current, currentPoint, top);
+                }
+                else
+                {
+                    HandleDifferentSide(outputLines, stack, currentPoint, top);
+                }
+            }
+        }
+
+        private static void HandleDifferentSide(List<Line> outputLines, Stack<Point> stack, Point currentPoint, Point top)
+        {
+            while (stack.Count != 1)
+            {
+                Point lastTop = stack.Pop();
+                outputLines.Add(new Line(currentPoint, lastTop));
+            }
+            stack.Pop();
+            stack.Push(top);
+            stack.Push(currentPoint);
+        }
+
+        private int HandleSameSide(List<Point> outputPoints, List<Line> outputLines, Polygon polygon, Stack<Point> stack, int current, Point currentPoint, Point top)
+        {
+            stack.Pop();
+            Point lastTop = stack.Peek();
+
+            int index = outputPoints.IndexOf(top);
+            if (IsConvex(polygon, index))
+            {
+                outputLines.Add(new Line(currentPoint, lastTop));
+                if (stack.Count == 1)
+                {
+                    stack.Push(currentPoint);
+                    current++;
+                }
+            }
+            else
+            {
+                stack.Push(top);
+                stack.Push(currentPoint);
+                current++;
+            }
+
+            return current;
+        }
+
+        private static bool CheckSameSide(List<Point> sortedPoints, Point currentPoint, Point top)
+        {
+            bool sameSide = false;
+            if (currentPoint.X < sortedPoints[0].X && top.X < sortedPoints[0].X)
+                sameSide = true;
+            else if (currentPoint.X > sortedPoints[0].X && top.X > sortedPoints[0].X)
+                sameSide = true;
+            return sameSide;
+        }
+
+        // Check the orientation of the polygon
+        public Polygon MakePolygonCounterClockwise(Polygon polygon)
+        {
+            double signedArea = 0;
+            for (int i = 0; i < polygon.lines.Count; i++)
+                signedArea += (polygon.lines[i].End.X - polygon.lines[i].Start.X) * (polygon.lines[i].End.Y + polygon.lines[i].Start.Y);
+            signedArea /= 2;
+
+            if (signedArea > 0)
+            {
+                polygon.lines.Reverse();
+                for (int i = 0; i < polygon.lines.Count; i++)
+                {
+                    Point temp = polygon.lines[i].Start;
+                    polygon.lines[i].Start = polygon.lines[i].End;
+                    polygon.lines[i].End = temp;
+                }
+            }
+            return polygon;
+        }
+
+        // Check Monotone: this function returns true if and only if no cusp points
+        public bool CheckMonotone(Polygon polygon)
+        {
+            int count = 0;
+
+            for (int i = 0; i < polygon.lines.Count; i++)
+            {
+                int previous = ((i - 1) + polygon.lines.Count) % polygon.lines.Count;
+                int next = (i + 1) % polygon.lines.Count;
+
+                Point currentPoint = polygon.lines[i].Start;
+                Point prevPoint = polygon.lines[previous].Start;
+                Point nextPoint = polygon.lines[next].Start;
+
+                if (nextPoint.Y < currentPoint.Y && prevPoint.Y < currentPoint.Y && !IsConvex(polygon, i))
+                    count++;
+                else if (nextPoint.Y > currentPoint.Y && prevPoint.Y > currentPoint.Y && !IsConvex(polygon, i))
+                    count++;
+            }
+
+            return count == 0;
+        }
+
+        // Check Convex point 
+        public bool IsConvex(Polygon polygon, int currentIndex)
+        {
+            int previous = ((currentIndex - 1) + polygon.lines.Count) % polygon.lines.Count;
+            int next = (currentIndex + 1) % polygon.lines.Count;
+
+            Point p1 = polygon.lines[previous].Start;
+            Point p2 = polygon.lines[currentIndex].Start;
+            Point p3 = polygon.lines[next].Start;
+            Line line = new Line(p1, p2);
+
+            return HelperMethods.CheckTurn(line, p3) == Enums.TurnType.Left;
+        }
+
+        // Sort the points based on max Y and max X in case of a tie O(n)
+        public static int PointSort(Point a, Point b)
+        {
+            if (a.Y == b.Y)
+                return -a.X.CompareTo(b.X);
+            else
+                return -a.Y.CompareTo(b.Y);
         }
 
         public override string ToString()
--- CGUI-master/CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
+++ CGUI-master/CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
@@ -1,21 +1,238 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CGUtilities;
 
 namespace CGAlgorithms.Algorithms.SegmentIntersection
 {
-    class SweepLine:Algorithm
+    public class SweepLine : Algorithm
     {
-        public override void Run(List<CGUtilities.Point> points, List<CGUtilities.Line> lines, List<CGUtilities.Polygon> polygons, ref List<CGUtilities.Point> outPoints, ref List<CGUtilities.Line> outLines, ref List<CGUtilities.Polygon> outPolygons)
-        {
 
+
+        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        {
+            // Preprocess lines to ensure Start.X is always less than End.X
+            lines = LineHandler.HandleLines(lines);
+            // Generate event points from the lines
+            List<(Point Point, string EventType, int Index)> eventPoints = EventPointGenerator.GenerateEventPoints(lines);
+
+            // Sort event points based on X-axis in descending order
+            eventPoints = EventPointSorter.SortEventPoints(eventPoints);
+            eventPoints.Reverse();
+
+            // Initialize data structures for the sweep line algorithm
+            List<(Point Point, string EventType, int Index)> activitySet = new List<(Point, string, int)>();
+            List<(Point Point, string EventType, int Index)> allBelow = new List<(Point, string, int)>();
+            List<(Point Point, string EventType, int Index)> allAbove = new List<(Point, string, int)>();
+
+            foreach (var eventPoint in eventPoints)
+            {
+                allBelow.Clear();
+                allAbove.Clear();
+
+                if (eventPoint.EventType == "StartPoint")
+                {
+                    activitySet.Add(eventPoint);
+
+                    foreach (var point in activitySet.FindAll(p => p != eventPoint))
+                    {
+                        if (point.Point.Y > eventPoint.Point.Y)
+                            allAbove.Add(point);
+                        else
+                            allBelow.Add(point);
+                    }
+
+                    // Check for intersections above and below the current point
+                    CheckAndAddIntersection(lines, eventPoint, allAbove, outPoints);
+                    CheckAndAddIntersection(lines, eventPoint, allBelow, outPoints);
+                }
+                else // EndPoint
+                {
+                    // Remove the corresponding StartPoint from the activity set
+                    int startIndex = activitySet.FindIndex(p => p.Index == eventPoint.Index && p.EventType != eventPoint.EventType);
+                    activitySet.RemoveAt(startIndex);
+
+                    foreach (var point in activitySet)
+                    {
+                        if (point.Point.Y > eventPoint.Point.Y)
+                            allAbove.Add(point);
+                        else
+                            allBelow.Add(point);
+                    }
+
+                    // Check for intersections above and below the current point
+                    CheckAndAddIntersection(lines, eventPoint, allAbove, outPoints);
+                    CheckAndAddIntersection(lines, eventPoint, allBelow, outPoints);
+                }
+            }
         }
 
+        // Check for intersections and add intersection points to the result list
+        private void CheckAndAddIntersection(List<Line> lines, (Point Point, string EventType, int Index) eventPoint, List<(Point Point, string EventType, int Index)> otherPoints, List<Point> outPoints)
+        {
+            foreach (var otherPoint in otherPoints)
+            {
+                if (DoIntersect(lines[eventPoint.Index], lines[otherPoint.Index]))
+                {
+                    Point intersectionPoint = ComputeIntersectionPoint(lines[eventPoint.Index], lines[otherPoint.Index]);
+                    if (!outPoints.Contains(intersectionPoint))
+                        outPoints.Add(intersectionPoint);
+                }
+            }
+        }
+        private bool DoIntersect(Line line1, Line line2)
+        {
+            // Extract points from lines
+            Point p1 = line1.Start;
+            Point q1 = line1.End;
+            Point p2 = line2.Start;
+            Point q2 = line2.End;
+
+            // Find orientations
+            int orientation1 = Orientation(p1, q1, p2);
+            int orientation2 = Orientation(p1, q1, q2);
+            int orientation3 = Orientation(p2, q2, p1);
+            int orientation4 = Orientation(p2, q2, q1);
+
+            // General case
+            if (orientation1 != orientation2 && orientation3 != orientation4)
+                return true;
+
+            // Special Cases
+
+            // p1, q1, and p2 are collinear and p2 lies on segment p1q1
+            if (orientation1 == 0 && OnSegment(p1, p2, q1))
+                return true;
+
+            // p1, q1, and q2 are collinear and q2 lies on segment p1q1
+            if (orientation2 == 0 && OnSegment(p1, q2, q1))
+                return true;
+
+            // p2, q2, and p1 are collinear and p1 lies on segment p2q2
+            if (orientation3 == 0 && OnSegment(p2, p1, q2))
+                return true;
+
+            // p2, q2, and q1 are collinear and q1 lies on segment p2q2
+            if (orientation4 == 0 && OnSegment(p2, q1, q2))
+                return true;
+
+            return false; // Doesn't fall in any of the above cases
+        }
+
+        private int Orientation(Point p, Point q, Point r)
+        {
+            double val = (q.Y - p.Y) * (r.X - q.X) - (q.X - p.X) * (r.Y - q.Y);
+
+            if (val == 0) return 0; // collinear
+            return (val > 0) ? 1 : 2; // clock or counterclock wise
+        }
+
+        private bool OnSegment(Point p, Point q, Point r)
+        {
+            return q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
+                   q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y);
+        }
+
+        private Point ComputeIntersectionPoint(Line line1, Line line2)
+        {
+            double x1 = line1.Start.X;
+            double y1 = line1.Start.Y;
+            double x2 = line1.End.X;
+            double y2 = line1.End.Y;
+
+            double x3 = line2.Start.X;
+            double y3 = line2.Start.Y;
+            double x4 = line2.End.X;
+            double y4 = line2.End.Y;
+
+            // Use the line intersection formula
+            double x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) /
+                       ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+
+            double y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) /
+                       ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+
+            return new Point(x, y);
+        }
         public override string ToString()
         {
             return "Sweep Line";
         }
     }
+
+
+    // Helper class to handle lines preprocessing
+    public static class LineHandler
+    {
+        public static List<Line> HandleLines(List<Line> unhandled)
+        {
+            // Ensure Start.X is always less than End.X
+            for (int i = 0; i < unhandled.Count; i++)
+            {
+                if (unhandled[i].Start.X > unhandled[i].End.X)
+                {
+                    Point temp = unhandled[i].Start;
+                    unhandled[i].Start = unhandled[i].End;
+                    unhandled[i].End = temp;
+                }
+            }
+            return unhandled;
+        }
+    }
+
+    // Helper class to sort event points based on X-axis
+    public static class EventPointSorter
+    {
+        public static List<(Point Point, string EventType, int Index)> SortEventPoints(List<(Point Point, string EventType, int Index)> unsortedList)
+        {
+            // Sort in descending order based on X-axis
+            unsortedList.Sort((p1, p2) => p2.Point.X.CompareTo(p1.Point.X));
+            return unsortedList;
+        }
+    }
+
+    // Helper class to find extreme points in Y-axis
+    public static class EventPointFinder
+    {
+        public static (Point Point, string EventType, int Index) FindMinY(List<(Point Point, string EventType, int Index)> above)
+        {
+            var minPoint = above[0];
+            for (int i = 1; i < above.Count; i++)
+            {
+                if (minPoint.Point.Y > above[i].Point.Y)
+                    minPoint = above[i];
+            }
+            return minPoint;
+        }
+
+        public static (Point Point, string EventType, int Index) FindMaxY(List<(Point Point, string EventType, int Index)> above)
+        {
+            var maxPoint = above[0];
+            for (int i = 1; i < above.Count; i++)
+            {
+                if (maxPoint.Point.Y < above[i].Point.Y)
+                    maxPoint = above[i];
+            }
+            return maxPoint;
+        }
+    }
+
+    // bn3ml alevent points wbn7dd kol no2tahya start wla end
+    public static class EventPointGenerator
+    {
+        public static List<(Point Point, string EventType, int Index)> GenerateEventPoints(List<Line> lines)
+        {
+            List<(Point Point, string EventType, int Index)> eventPoints = new List<(Point, string, int)>();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                // Generate StartPoint and EndPoint for each line
+                eventPoints.Add((lines[i].Start, "StartPoint", i));
+                eventPoints.Add((lines[i].End, "EndPoint", i));
+            }
+
+            return eventPoints;
+        }
+    }
+
 }
